cbfee73aab30ae4572c08a4919cace1decdd2f87
Ayodya-erandi <66939752+Ayodya-erandi@users.noreply.github.com>
2021-01-13 22:48:16 +0530

    Add files via upload

--- web-app/src/components/Emergency/Emergency.jsx
+++ web-app/src/components/Emergency/Emergency.jsx
@@ -1,37 +1,39 @@
 import React, {Component} from 'react';
 import "./Emergency.css";
 
+const styleObj = {fontSize: 50,color: "white",}
 const styleObject = {color: "white"}
-const Sobject = {color: "white",fontSize: 30, top:"20%"}
+const stylenum = {fontSize: 40,color:"white"}
 
 class Emergency extends Component {
     render() {
         return (
             <div className="parent">
-                <div className="parent L"></div>
+                <div className="parent L-details">
                     <div class="vl"></div>
-                    <div className="content"><p >Lorem ipsum dolor sit amet consectetur adipisicing elit. A unde aut molestiae beatae sed reprehenderit nobis. Totam, dolorem, ad voluptatem officiis obcaecati aliquid hic deserunt voluptatum veniam eos, veritatis aperiam?</p>
+                    <div className="title"><h1 style={styleObj}>Emergency Numbers</h1></div>
+                    <div className="details"><p style={styleObject}><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. A unde aut molestiae beatae sed reprehenderit nobis. Totam, dolorem, ad voluptatem officiis obcaecati aliquid hic deserunt voluptatum veniam eos, veritatis aperiam?</strong></p></div>
                 </div>
                 <div className="parent R">
                     <div class="container-fluid">
                         <div class="row">
-                            <div className="col-sm-6" style={{backgroundColor: '#F08080'}}>
-                                <p style={Sobject}>110</p><p style={styleObject}>Ambulance</p>   
+                            <div className="col-sm-6" style={{backgroundColor: '#f88379'}}>
+                                <p style={stylenum}>110</p><p style={styleObject}>Ambulance</p>   
                             </div>
                             <div className="col-sm-6" style={{backgroundColor: '#FF4500'}}>
-                                <p style={styleObject}>111<br/>Fire and Rescue</p>    
+                            <p style={stylenum}>111</p><p style={styleObject}>Fire and Rescue</p>    
                             </div>
                             <div className="col-sm-6" style={{backgroundColor: '#FF4500'}}>
-                                <p style={{color:'white',lineHeight:'50px'}}>1933<br/>Sri Lanka Police Department(CID Operations unit)</p>    
+                                <p style={stylenum}>1933</p><p style={styleObject}>Sri Lanka Police Department(CID Operations unit)</p>    
                             </div>
-                            <div className="col-sm-6" style={{backgroundColor: '#F08080'}}>
-                                <p style={styleObject}>119<br/>Police Emergency Service</p>    
+                            <div className="col-sm-6" style={{backgroundColor: '#f88379'}}>
+                                <p style={stylenum}>119</p><p style={styleObject}>Police Emergency Service</p>    
                             </div>
-                            <div className="col-sm-6" style={{backgroundColor: '#F08080'}}>
-                                <p style={styleObject}>118<br/>National Help Desk(Ministry Of Defence)</p>  
+                            <div className="col-sm-6" style={{backgroundColor: '#f88379'}}>
+                                <p style={stylenum}>118</p><p style={styleObject}>National Help Desk(Ministry Of Defence)</p>  
                             </div>
                             <div className="col-sm-6" style={{backgroundColor: '#FF4500'}}>
-                                <p style={styleObject}>115<br/>Colombo Municipal Council-Operation Unit</p>   
+                                <p style={stylenum}>115</p><p style={styleObject}>Colombo Municipal Council-Operation Unit</p>   
                             </div>
                         </div>
                     </div>
