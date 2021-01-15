f8384bfeb7cd6a7622d256b1a8eab00f720cea96
Ayodya-erandi <66939752+Ayodya-erandi@users.noreply.github.com>
2021-01-15 17:28:09 +0530

    Add files via upload

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router,Route, Switch } from 'react-router-dom'
-import ComplaintForm from './components/complaint/complaint'
 import Aboutus from './components/Aboutus/Aboutus';
 import Emergency from './components/Emergency/Emergency';
 import NavBar from './components/NavBar/NavBar';
 import LawTalk from './components/LawTalk/LawTalk';
+import ComplaintForm from "./components/ComplaintForm/ComplaintForm";
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
+      <NavBar/>
       <Switch>
-        <Route path='/complaint' component={ComplaintForm} />
         <Route path='/AboutUs' component={Aboutus} />
         <Route path='/EmergencyNumbers' component={Emergency} />
         <Route path='/lawTalk' component={LawTalk} />
+        <Route path='/complaint' component={ComplaintForm} />
       </Switch>
     </Router>
   );
