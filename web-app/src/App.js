5ae3b94dccf003460bda32cb6dda1b2ef430253e
Ayodya-erandi <66939752+Ayodya-erandi@users.noreply.github.com>
2021-01-17 22:51:14 +0530

    Add files via upload

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar/NavBar';
 import LawTalk from './components/LawTalk/LawTalk';
 import ComplaintForm from "./components/ComplaintForm/ComplaintForm";
 import LoginForm from "./components/Login/Login"
+import Home from "./components/Home/Home"
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/lawTalk' component={LawTalk} />
         <Route path='/complaint' component={ComplaintForm} />
         <Route path='/Login' component={LoginForm} />
+        <Route path='/Home' component={Home} />
       </Switch>
     </Router>
   );
