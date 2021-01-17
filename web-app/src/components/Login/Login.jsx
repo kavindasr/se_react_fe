13304912efaddf88af3a9c25c8a99346f2c001de
Ayodya-erandi <66939752+Ayodya-erandi@users.noreply.github.com>
2021-01-17 16:38:40 +0530

    Add files via upload

---
+++ web-app/src/components/Login/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import CustomForm from './CustomForm';
+import "./Login.css"
+
+class LoginForm extends CustomForm {
+    
+    render() {
+        return (
+            <div className="page">
+                <Form className="Form">
+                    <h1 className = 'heading' >Login</h1>
+                    {this.renderFormInput(
+                        {   controlId: "formBasicEmail", 
+                            label: "Email Address", 
+                            type:'email', 
+                            placeholder:'Enter Your Email',
+                            size: 'lg'
+                    
+                        }) 
+                    }
+                    {this.renderFormInput(
+                        {   controlId: "formBasicPassword", 
+                            label: "Password", 
+                            type:'password', 
+                            placeholder:'Enter Your Password', 
+                            size: 'lg'
+                        }) 
+                    }
+                    <Button>Log in</Button>
+                </Form>   
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
